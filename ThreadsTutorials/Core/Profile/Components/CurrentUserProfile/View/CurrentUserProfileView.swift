6f2ac44a9caeaa26c2f93b2402e419fe97099c8b
YuriyIzbash <izbash91@gmail.com>
2024-09-28 22:41:40 +0200

    Fixed Feed and Added User Threads

---
+++ ThreadsTutorials/Core/Profile/Components/CurrentUserProfile/View/CurrentUserProfileView.swift
@@ -0,0 +1,66 @@
+//
+//  CurrentUserProfileView.swift
+//  ThreadsTutorials
+//
+//  Created by YURIY IZBASH on 25. 9. 24.
+//
+
+import SwiftUI
+
+struct CurrentUserProfileView: View {
+    @StateObject var viewModel = CurrentUserProfileViewModel()
+    @State private var showEditProfile = false
+    
+    private var currentUser:User? {
+        return viewModel.currentUser
+    }
+    
+    var body: some View {
+        NavigationStack {
+            ScrollView(showsIndicators: false) {
+                //bio and stats
+                VStack(spacing: 20) {
+                    ProfileHeaderView(user: currentUser)
+                    
+                    Button {
+                        showEditProfile.toggle()
+                    } label: {
+                        Text("Edit profile")
+                            .font(.subheadline)
+                            .fontWeight(.semibold)
+                            .foregroundColor(.black)
+                            .frame(width: 352, height: 32)
+                            .background(.white)
+                            .cornerRadius(8)
+                            .overlay{
+                                RoundedRectangle(cornerRadius: 8)
+                                    .stroke(Color(.systemGray4), lineWidth: 1)
+                            }
+                    }
+                    //user content list view
+                    UserContentListView()
+                }
+            }
+            .sheet(isPresented: $showEditProfile, content: {
+                if let user = currentUser {
+                    EditProfileView(user: user)
+                }
+            })
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button {
+                        AuthService.shared.signOut()
+                    } label: {
+                        Image(systemName: "line.3.horizontal")
+                    }
+                    
+                }
+            }
+            .padding(.horizontal)
+        }
+    }
+}
+
+#Preview {
+    CurrentUserProfileView()
+}
---
+++ ThreadsTutorials/Core/Profile/Components/ViewModel/UserContentListViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  UserContentListViewModel.swift
+//  ThreadsTutorials
+//
+//  Created by YURIY IZBASH on 28. 9. 24.
+//
+
+import Foundation
